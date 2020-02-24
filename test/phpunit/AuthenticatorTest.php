4ad2eeae1a2dede7f33f79555631d92216e8046e
Greg Bowler <greg.bowler@g105b.com>
2020-02-24 15:15:30 +0000

    Add fake test to get test cases executing

---
+++ test/phpunit/AuthenticatorTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Authwave\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class AuthenticatorTest extends TestCase {
+	public function testOnePlusOne() {
+		self::assertEquals(2, 1 + 1);
+	}
+}
