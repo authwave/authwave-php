146620bdb586d7d9458e2f525cca29e2d5ea1c77
Greg Bowler <greg.bowler@g105b.com>
2020-02-29 21:03:25 +0000

    Test SessionData class

---
+++ test/phpunit/SessionDataTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace Authwave\Test;
+
+use Authwave\NotLoggedInException;
+use Authwave\SessionData;
+use Authwave\Token;
+use Authwave\UserData;
+use PHPUnit\Framework\TestCase;
+
+class SessionDataTest extends TestCase {
+	public function testGetTokenNull() {
+		$sut = new SessionData();
+		self::expectException(NotLoggedInException::class);
+		$sut->getToken();
+	}
+
+	public function testGetToken() {
+		$token = self::createMock(Token::class);
+		$sut = new SessionData($token);
+		self::assertSame($token, $sut->getToken());
+	}
+
+	public function testGetUserDataNull() {
+		$sut = new SessionData();
+		self::expectException(NotLoggedInException::class);
+		$sut->getUserData();
+	}
+
+	public function testGetUserData() {
+		$token = self::createMock(Token::class);
+		$userData = self::createMock(UserData::class);
+		$sut = new SessionData($token, $userData);
+		self::assertSame($userData, $sut->getUserData());
+	}
+}
