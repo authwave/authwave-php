ea4d875b7f0623402220b647170f7e5601d8eebb
Greg Bowler <greg.bowler@g105b.com>
2020-03-01 14:49:40 +0000

    Ignore coverage on RedirectHandler.php

--- src/RedirectHandler.php
+++ src/RedirectHandler.php
@@ -3,6 +3,7 @@ namespace Authwave;
 
 use Psr\Http\Message\UriInterface;
 
+/** @codeCoverageIgnore */
 class RedirectHandler {
 	public function redirect(UriInterface $uri, int $code = 303):void {
 		header("Location: $uri", true, $code);
