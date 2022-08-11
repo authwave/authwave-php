b0e50336f40919be29ced5ece26ae0abf9c3246e
Greg Bowler <greg.bowler@g105b.com>
2022-08-11 13:28:20 +0100

    feature: user class

---
+++ src/User.php
@@ -0,0 +1,14 @@
+<?php
+namespace Authwave;
+
+class User {
+	public function __construct(
+		public readonly string $id,
+		public readonly string $email,
+		private readonly array $kvp = [],
+	) {}
+
+	public function getData(string $key):?string {
+		return $this->kvp[$key] ?? null;
+	}
+}
